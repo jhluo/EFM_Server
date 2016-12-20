03cad462697b7093db22789f58a6263bc500d205
seanxiezhou <seanxiezhou@sina.com>
2016-12-20 10:48:49 +0800

    Change Title

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -21,7 +21,7 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    setWindowTitle("电场仪接收服务器");
+    setWindowTitle("负离子接收服务器");
     m_pServer = new TheServer(this);
 
     setupGui();
